fb725f51471ba4d5d2b4a93cf408a281903d963d
Roman Kolesnikov <romalev2006@mail.ru>
2026-01-19 20:33:24 +0300

    feat: add base routing for pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './styles/App.css';
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+
+import Home from './pages/Home/Home';
+import Menu from './pages/Menu/Menu';
+import Stocks from './pages/Stocks/Stocks';
+import Gallery from './pages/Gallery/Gallery';
+import Partners from './pages/Partners/Partners';
+import Contacts from './pages/Contacts/Contacts';
+import Events from './pages/Events/Events';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/menu" element={<Menu/>}/>
+        <Route path="/stocks" element={<Stocks/>}/>
+        <Route path="/gallery" element={<Gallery/>}/>
+        <Route path="/partners" element={<Partners/>}/>
+        <Route path="/contacts" element={<Contacts/>}/>
+        <Route path="/events" element={<Events/>}/>
+      </Routes>
+      <Footer/>
+    </Router>
   );
 }
 

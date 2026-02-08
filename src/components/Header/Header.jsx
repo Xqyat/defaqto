9653df96d4856be56e5acce0d44b70468feaa1f3
Roman Kolesnikov <romalev2006@mail.ru>
2026-02-08 13:28:26 +0300

    feat: complete header with assets

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,34 +1,44 @@
 import { NavLink } from "react-router-dom";
+import LogoBlack from "../../assets/images/DeFaqtoLogoBlack.png";
+import HeaderVideo from "../../assets/videos/HeaderVideo.mp4";
 import "./Header.css";
 
 function Header(){
     return(
         <>
-        <header>
-            <nav>
-                <NavLink to='/'>
-                    Главная
-                </NavLink>
-                <NavLink to='/menu'>
-                    Меню
-                </NavLink>
-                <NavLink to='/stocks'>
-                    Акции
-                </NavLink>
-                <NavLink to='/gallery'>
-                    Фотоотчёты
-                </NavLink>
-                <NavLink to='/partners'>
-                    Партнёры
-                </NavLink>
-                <NavLink to='/contacts'>
-                    Контакты
-                </NavLink>
-                <NavLink to='/events'>
-                    Афиша
-                </NavLink>
-            </nav>
-        </header>
+            <header>
+                <div className="header-infobar">
+                    <div>
+                        <p>Большая Лубянка, 30/2</p>
+                        <p>Сегодня до 06:00</p>
+                    </div>
+                    <p>ЗАКАЗЫ: +7 495 624 44 97</p>
+                </div>
+                <div className="header-main">
+                    <div className="logo">
+                        <NavLink to='/'>
+                        <img src={LogoBlack} alt="DeFAQto" className="logo-black" style={{ width: 366, height: 110 }}/>
+                        </NavLink>
+                    </div>
+                    <nav>
+                        <ul>
+                            <li><NavLink to='/'>Главная</NavLink></li>
+                            <li><NavLink to='/events'>Афиша</NavLink></li>
+                            <li><NavLink to='/menu'>Меню</NavLink></li>
+                            <li><NavLink to='/gallery'>Галерея</NavLink></li>
+                            <li><NavLink to='/contacts'>Контакты</NavLink></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div className="header-hero">
+                    <video className="hero-video"autoPlay muted loop playsInline 
+                    style={{ width: '100%', height: '518px', objectFit: 'cover'}}>
+                    <source src={HeaderVideo} type="video/mp4"/>
+                    </video>
+                    {/* Нужно сделать запасной вариант */}
+                    <button>Забронировать</button>
+                </div>
+            </header>
         </>
     )
 }

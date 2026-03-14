7d029f0801009db77f3c488c9624e5f42cc07a8c
Roman Kolesnikov <romalev2006@mail.ru>
2026-03-14 16:36:26 +0300

    feat: add BookingFormModal in header and do some minor changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
+import React, { useState } from 'react';
 import { NavLink } from "react-router-dom";
 import { useLocation } from 'react-router-dom';
 import LogoBlack from "../../assets/images/DeFaqtoLogoBlack.png";
 import HeaderVideo from "../../assets/videos/HeaderVideo.mp4";
 import Button from "../Button/Button.jsx";
 import MobileMenu from '../MobileMenu/MobileMenu.jsx';
+import BookingFormModal from '../BookingFormModal/BookingFormModal.jsx'
 import "./Header.css";
 
 
@@ -11,15 +13,21 @@ import "./Header.css";
 function Header(){
     const { pathname } = useLocation();
     const isHome = pathname === '/';
+
+    
+    const [isOpen, setIsOpen] = useState(false);
+    const handleBookingSubmit = (data) => {
+        console.log(data);
+      };
     return(
         <>
             <header>
                 <div className="header-infobar">
-                    <div>
-                        <p>Большая Лубянка, 30/2</p>
-                        <p>Сегодня до 06:00</p>
+                    <div className="header-infobar-address_and_time">
+                        <p className="header-infobar-address">Большая Лубянка, 30/2</p>
+                        <p className="header-infobar-time">Сегодня до 06:00</p>
                     </div>
-                    <p>ЗАКАЗЫ: +7 495 624 44 97</p>
+                    <p className="header-infobar-phone">ЗАКАЗЫ: +7 495 624 44 97</p>
                 </div>
                 <div className="header-main">
                     <div className="logo">
@@ -50,7 +58,8 @@ function Header(){
                         <source src={HeaderVideo} type="video/mp4" alt="Фоновое видео бара DeFAQto с атмосферным освещением"/>
                         </video>
                     )}
-                    <Button>Забронировать</Button>
+                    <Button onClick={() => setIsOpen(true)}>Забронировать</Button>
+                    <BookingFormModal isOpen={isOpen} onClose={() => setIsOpen(false)} onSubmit={handleBookingSubmit}/>
                 </div>
             </header>
         </>

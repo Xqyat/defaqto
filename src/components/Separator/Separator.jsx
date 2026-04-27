ecb623baa1349e27ce78d8140a531fe5b136423d
Roman Kolesnikov <romalev2006@mail.ru>
2026-04-27 23:55:15 +0300

    feat: add Separator component

---
+++ src/components/Separator/Separator.jsx
@@ -0,0 +1,11 @@
+import "./Separator.css"
+import Texture from "../../assets/images/DefaqtoTexture.png"
+const Separator = () => {
+    return (
+        <section className="separator">
+            <img src={Texture} alt="" />
+        </section>
+    )
+}
+
+export default Separator

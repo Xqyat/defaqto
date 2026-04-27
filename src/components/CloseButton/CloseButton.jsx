21569de4e5a70a0c13d10697028d43e47fc6f7a9
Roman Kolesnikov <romalev2006@mail.ru>
2026-04-27 12:45:38 +0300

    refactor: change x symbol on cross icon

--- src/components/CloseButton/CloseButton.jsx
+++ src/components/CloseButton/CloseButton.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import './CloseButton.css';
 
 const CloseButton = ({ onClick }) => (
-  <button className="close_button" onClick={onClick}>×</button>
+  <button className="close_button" onClick={onClick}>
+    <svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M21.5 1.5L1.5 21.5M1.50002 1.5L21.5 21.5" stroke="#2B2B2B" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  </button>
 );
 
 export default CloseButton;
